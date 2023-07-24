b80668a51fe070202e522099d213ff056e437f02
DispicableLee <lee.robert053@gmail.com>
2023-07-24 12:30:38 -0400

    added vectors, updated BE

--- coven/src/components/UploadNewProject.js
+++ coven/src/components/UploadNewProject.js
@@ -34,8 +34,9 @@ export default function UploadNewProject(){
         console.log(e.target.files[0])
         setSelectedFile(e.target.files[0])
         const objectUrl = {image: URL.createObjectURL(selectedFile)}
-        console.log(objectUrl)
-        setFormImage(objectUrl)
+        console.log(objectUrl.image)
+        setPreview(objectUrl.image)
+        setFormImage(objectUrl.image)
     }
 // =========================== handleFormSubmit ========================================
 // 
@@ -67,58 +68,40 @@ export default function UploadNewProject(){
 // ============================ return ===============================================
     return (
         <div id="container-box" >
-            <div className="title">
-                <h1 className="heading-one">
-                    Upload New Project
-                </h1>
+            <div className="title animate-enter">
+                <h1>Upload New Project</h1>
             </div>
             <div id="input-fields">
-
-                <section>
-                    <h2
-                        className="heading-two"
-                    >
-                    What is the name of your project?</h2>
+{/* ================================== form inputs ========== */}
+                <section className="text-input-field">
+                    <h2> What is the name of your project? </h2>
                     <input 
                         type="text" 
-                        id="project-name-input"
                         onChange={(e)=>setFormName(e.target.value)}
                         />
                 </section>
                 
-                <section>
-                    <h2
-                    className="heading-two"
-                    >
-                        provide a description for your project:</h2>
-                    <textarea 
-                        id="project-description-input"
-                        onChange={(e)=>setFormDescription(e.target.value)}
-                        />
-
+                <section className="text-input-field">
+                    <h2>provide a description for your project:</h2>
+                    <textarea onChange={(e)=>setFormDescription(e.target.value)}/>
                 </section>
 
-                <section className="website-link">
+                <section className="text-input-field">
                     <h2>does your project have an external link?</h2>
                     <input 
                         type="text" 
-                        id="project-link"
                         onChange={(e)=>setFormLink(e.target.value)}
                         />
                 </section>
 {/* ========================== Image Handling ============================= */}
                 <section className="project-image">
-                    <h2
-                        className="heading-two"
-                    >select an image for your project</h2>
-                    <div id="image-handler">
+                    <h2>select an image for your project</h2>
                         <label className="file-handle">
                             <input 
                                 type="file" 
                                 onChange={onSelectFile}
                                 />
                         </label>
-                    </div>
                 </section>
             </div>
 {/* ==================== pressing submit button ===============================*/}
@@ -131,12 +114,36 @@ export default function UploadNewProject(){
                         <h1>Submit</h1>
                     </section>: finalSubmitButton}
 {/* ===================== final submission preview =======================*/}
-                <div id="preview-box">
-                    <h3>hi</h3>
 
-                </div>
+                {finalSubmit?                
+                    <div id="preview-box">
+                        <div className="preview-image-contributors">
+                            <img src={preview}/>
+                            <aside className="contributors-row">
 
+                            </aside>
+                        </div>
 
+                        <section id="preview-description-info">
+                            <div>
+                                <h2>title</h2>
+                                <p>
+                                    Non aliqua cillum qui laborum eiusmod eu. 
+                                    Cillum minim nulla cupidatat reprehenderit laborum sit fugiat culpa ut et est dolore. 
+                                    Elit fugiat ullamco commodo laboris cupidatat magna irure id. 
+                                    Do eu consectetur id ut sint.
+                                    Aute labore aute pariatur reprehenderit et laborum ut proident laboris adipisicing officia ut voluptate commodo. 
+                                    Eiusmod reprehenderit ea reprehenderit ea sit cupidatat sit voluptate eiusmod ex dolor non elit.
+                                </p>
+                            </div>
+                            <div>
+                                <h2>link</h2>
+                            </div>
+                        </section>
+                    </div>      
+                    :
+                     <></>
+                }
         </div>
     )
 
